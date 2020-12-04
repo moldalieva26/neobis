743dea99a6b3218558bc3f0e7833976cd41c8f25
Meerim <moldalieva26@gmail.com>
2020-12-04 17:01:24 -0500

    Tried to deploy a demo app with webapp runner

---
+++ week12/webapp-deploy/src/main/java/webapp/webappdeploy/controller/DemoController.java
@@ -0,0 +1,13 @@
+package webapp.webappdeploy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+      
+      @GetMapping("/check")
+      public String check() {
+           return "Hello Bitches!";
+      }
+}
